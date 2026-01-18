9e4ed030bb8818aae6635976df65065a7ae0787b
Yuri Rene <yurirene@gmail.com>
2026-01-17 23:10:50 -0300

    testando

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>SICOM - @yield('title', 'Dashboard')</title>
+    <title>Conexão Missionária - @yield('title', 'Dashboard')</title>
 
     <link rel="shortcut icon" href="/assets/svg/favicon.svg" type="image/x-icon">
     <link rel="shortcut icon"
